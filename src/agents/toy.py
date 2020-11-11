c077884979a31905a500baf7e0c75cfa9ab9cad1
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-11 14:10:38 -0500

    [agent] refs #2 - Toy agent
    Toy is an agent that only babies an other toys can push

---
+++ src/agents/toy.py
@@ -0,0 +1,15 @@
+from .base_agent import Agent
+from ..enviroment import EnvError, EnvTags
+
+class Toy(Agent):
+    def push(xdir, ydir, agent):
+        try:
+            assert agent.tag in [EnvTags.BABY, EnvTags.TOY] 
+            x, y = self.position
+            x, y = x + xdir, y + ydir
+            assert self.env.get(x, y).push(xdir, ydir, self):
+            self.set_position(x, y)
+            return True
+        except AssertionError: pass
+        except EnvError: pass
+        return False
---
+++ src/enviroment/utils.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class EnvTags(Enum):
+    EMPTY, BABY, BOT, TOY = range(4) 
