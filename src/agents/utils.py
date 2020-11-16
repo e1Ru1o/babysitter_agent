d398ee56cb8e50963d7aa96ee4de47dbcf960a25
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-16 17:38:03 -0500

    [agents] - Util methods for explore the enviroment
    Added bfs and get_path

--- src/agents/utils.py
+++ src/agents/utils.py
@@ -8,3 +8,42 @@ def random_dir(data=DIR):
 
 def select(agents, tag):
     return [x for x in agents if x.tag() == tag]
+
+def bfs(start, table, valid_tags):
+    q, idx = [start], 0
+    step = [[None] * len(x) for x in table]
+    visited = [[False] * len(x) for x in table]
+    distance = [[float('inf')] * len(x) for x in table]
+    x, y = start
+    distance[x][y] = 0
+    visited[x][y] = True
+    data = {tag:[] for tag in valid_tags}
+    while idx < len(q):
+        x, y = q[idx]
+        for xdir, ydir in DIR:
+            nx, ny = x + xdir, y + ydir
+            try:
+                assert nx >= 0 and ny >= 0
+                assert not visited[nx][ny]
+                tag = table[nx][ny].tag()
+                assert tag in valid_tags
+                d = distance[nx][ny] = distance[x][y] + 1
+                data[tag].append((nx, ny))
+                visited[nx][ny] = True
+                step[nx][ny] = (xdir, ydir)
+                q.append((nx, ny))
+            except IndexError: pass
+            except AssertionError: pass
+        idx += 1
+    return data, distance, step
+    
+def get_path(end, steps):
+    l = []
+    x, y = end
+    while steps[x][y]:
+        l.append(step[x][y])
+        xdir, ydir = step[x][y]
+        x -= xdir
+        y -= xdir
+    l.reverse()
+    return l
