d932d4986ae94a69c75fdb0b7cc0c9dcc8464de9
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-16 17:40:26 -0500

    [agents] - Basic Robot perception

--- src/agents/robot.py
+++ src/agents/robot.py
@@ -1,4 +1,8 @@
 from .base_agent import Agent
+from .utils import bfs, get_path
+from ..enviroment import EnvTags as Tags
+
+valid = [Tags.EMPTY, Tags.DIRTY, Tags.ROLLER, Tags.BABY]
 
 class Robot(Agent):
     '''
@@ -8,8 +12,11 @@ class Robot(Agent):
         super().__init__(**kwargs)
         self.carry = None
 
-    def action(self):
-        # //TODO: Implement
-        pass
+    def see(self):
+        return self.env.data
+
+    def perceive(self):
+        return bfs(self.position, self.see(), valid)
+        
 
     
