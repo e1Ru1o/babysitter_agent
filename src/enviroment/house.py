ac8519c65ecca432c5f26592b787f2b4f00f0128
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-16 22:19:24 -0500

    [env] - Better house implentation

--- src/enviroment/house.py
+++ src/enviroment/house.py
@@ -1,8 +1,8 @@
 import random
 from .error import EnvError
 from .base_env import Enviroment
-from .utils import EnvTags, EnvStatus
 from ..agents import Baby, Robot, Cell, Toy, Roller, DIR
+from .utils import EnvTags, EnvStatus, generate, fill_agents, agent_maker as maker
 
 class House(Enviroment):
     def __init__(self, n, m, t, babies, toys, dirty):
@@ -27,23 +27,26 @@ class House(Enviroment):
         super().run()
 
     def variate(self, rollers=None, toys=None, babies=None, dirty=None, bot=None):
-        def generate(c, tag, sz, current):
-            if current is None or len(current) != sz:
-                return [c(env=self, tag=tag) for _ in range(sz)]
-            return current
+        # New empty table
+        self.clear()
 
+        # Generate all the positions
         n, m = self.size
         positions = [(x, y) for x in range(n) for y in range(m)]
-        for pos in positions:
-            Cell(env=self, tag=EnvTags.EMPTY).set_position(*pos)
-        a, b = random.randint(0, n - 1), random.randint(0, m - 1)
-        dir = DIR.copy()
+        random.shuffle(positions)
+
+        # Set the rollers
+        dir = DIR[1:]
         random.shuffle(dir)
-        dir.remove((0, 0))
+        a, b = random.randint(0, n - 1), random.randint(0, m - 1)
+        rollers = generate(maker(Roller, self, EnvTags.ROLLER), self.babies, rollers)
         amount = self.babies - 1
-        rollers = generate(Roller, EnvTags.ROLLER, self.babies, rollers)
         for xdir, ydir in dir:
-            if self.valid_pos(a + xdir * amount, b + ydir * amount):
+            x, y = (min(n - 1, a + xdir * amount), min(m - 1, b + ydir * amount))
+            r = amount - (x - a + y - b)
+            first = (a - xdir * r, b - ydir * r)
+            if self.valid_pos(*first) and self.valid_pos(x, y):
+                a, b = first
                 for roller in rollers:
                     roller.set_position(a, b)
                     positions.remove((a, b))
@@ -51,24 +54,44 @@ class House(Enviroment):
                     b += ydir
                 break
 
-        toys = generate(Toy, EnvTags.TOY, self.toys, toys)
-        babies = generate(Baby, EnvTags.BABY, self.babies, babies)
-        dirty = generate(Cell, EnvTags.DIRTY, self.dirty, dirty)
-        bot = generate(Robot, EnvTags.BOT, 1, bot)
-        all_agents = [*toys, *babies, *dirty, *bot]
-        selected = random.sample(positions, len(all_agents))
-        for pos, agent in zip(selected, all_agents):
+        # Genarate all the objects
+        toys = generate(maker(Toy, self, EnvTags.TOY), self.toys, toys)
+        babies = generate(maker(Baby, self, EnvTags.BABY), self.babies, babies)
+        dirty = generate(maker(Cell, self, EnvTags.DIRTY), self.dirty, dirty)
+        bot = generate(maker(Robot, self, EnvTags.BOT), 1, bot)
+        callback = lambda: Cell(env=self, tag=EnvTags.EMPTY)
+        all_agents = fill_agents([*toys, *babies, *dirty, *bot], callback)
+        
+        # Assign positions
+        for pos, agent in zip(positions, all_agents):
             agent.set_position(*pos)
+
+        # Update the env
         self.build_data(rollers=rollers, toys=toys, babies=babies, dirty=dirty, bot=bot)  
         self.agents = [*bot, *babies]
 
+        # Add empty cells in the agents positions
+        for x in self.agents:
+            callback().set_position(*x.position)
+
     def set(self, x, y, agent):
-        self.get(x, y)
-        self.env[x][y] = agent
-        if not agent.position is None:
-             Cell(env=self, tag=EnvTags.EMPTY).set_position(*agent.position)
+        self.unset(agent)
+        cell = self._get(x, y)
+        cell.append(agent)
+        cell.sort(key=lambda x: x.tag().value)
         agent.position = (x, y)
+
+    def unset(self, agent):
+        if agent.position:
+            self._get(*agent.position).remove(agent)
+            agent.position = None
+
+    def _get(self, x, y):
+        return super().get(x, y)
             
+    def get(self, x, y):
+        return super().get(x, y)[-1]
+
     def stop(self):
         n, m = self.size
         cells = n * m
--- src/enviroment/utils.py
+++ src/enviroment/utils.py
@@ -1,7 +1,21 @@
 from enum import Enum
 
 class EnvTags(Enum):
-    EMPTY, BABY, BOT, TOY, DIRTY, ROLLER, FULL_ROLLER = range(7) 
+    EMPTY, DIRTY, TOY, ROLLER, FULL_ROLLER, BABY, BOT = range(7) 
 
 class EnvStatus(Enum):
     RUNNING, FIRED, CLEAN, TLE = range(4) 
+
+def agent_maker(c, env, tag):
+    return lambda: c(env=env, tag=tag)
+
+def generate(maker, sz, current):
+    if current is None or len(current) != sz:
+        return [maker() for _ in range(sz)]
+    return current
+
+def fill_agents(agents, callback):
+    for x in agents:
+        yield x
+    while True:
+        yield callback()
