52ffede941d5171f1eab419ef626a117a9a0ae34
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-18 18:43:09 -0500

    [agent] - Add proactive agent

---
+++ src/agents/proactive.py
@@ -0,0 +1,132 @@
+from .robot import Robot
+from .utils import get_path
+from ..enviroment import EnvTags as Tags
+from ..logging import Logger
+
+class Proactive(Robot):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.logger = Logger('App').getChild('Proactive')
+        self.plan   = None
+
+    def action(self):
+        steps = 1 if self.carry is None else 2
+        self.logger.debug(f'Bot action started, {steps} steps available', 'action')
+        for _ in range(steps):
+            try:
+                while True:
+                    # current position action
+                    assert not self.cur_pos_action()
+
+                    # use planed actions
+                    if self.plan:
+                        assert not self.do_planned_action()
+                    
+                    # New plan needed
+                    assert self.build_new_plan()
+            except AssertionError: pass
+            if self.carry is None: break
+        
+    def cur_pos_action(self):
+        agent = self.env.get(*self.position, -2)
+        if self.carry and (agent.tag() == Tags.ROLLER):
+            self.logger.info('Leaving the baby...', 'action')
+            self.carry = None
+            agent.lock(self)
+            return True
+        if agent.tag() == Tags.DIRTY:
+            self.logger.info('Cleaning the dirty...', 'action')
+            self.env.dirty -= 1
+            agent.tag(Tags.EMPTY)
+            return True
+        return False
+
+    def do_planned_action(self):
+        pos, tag = self.plan.pop(0)
+        agent = self.env.get(*pos)
+        if agent.tag() != tag:
+            self.logger.info('Agent planification failure', 'action')
+            return False
+        self.set_position(*pos)
+        if tag == Tags.BABY:
+            self.logger.info('Picking up a baby', 'action')
+            self.carry = agent
+            agent.lock(self)
+        return True
+
+    def build_new_plan(self):
+        #perception
+        perception = self.perceive()
+        try:
+            #goal layers
+            assert not self.do_roller(perception)
+            assert not self.do_baby(perception)
+            assert not self.do_dirty(perception)
+            return False
+        except AssertionError:
+            return True
+
+    def find(self, data, dis):
+        # Select the nearest agent
+        a, b = data[0]
+        for x, y in data:
+            if dis[x][y] < dis[a][b]:
+                a, b = x, y
+        agent = self.env.get(a, b)
+        self.logger.info(f'Nearest {str(agent).strip()} found', 'action')
+        return self.env.get(a, b)
+
+    def do_roller(self, perception):
+        # Precondition
+        if self.carry is None:
+            return False
+
+        # Body
+        data, dis, step = perception
+        data = data[Tags.ROLLER]
+        if not data:
+            # //TODO: Leave the baby if is not possible
+            # to clean the dirty carrying a baby
+            self.logger.debug('Search for a roller failed', 'action')
+            return False        
+        
+        roller = self.find(data, dis)
+        self.save_plan(roller.position, step)
+        return True
+            
+    def do_baby(self, perception):
+        # Precondition 
+        data, dis, step = perception
+        # //TODO: If not possible to clean dirty 
+        # carrying a baby, check also the rollers
+        if self.carry or (not data[Tags.BABY]):
+            return False
+
+        # Body
+        data = data[Tags.BABY]
+        baby = self.find(data, dis)
+    
+        self.save_plan(baby.position, step)
+        return True
+
+    def do_dirty(self, perception):
+         # Precondition 
+        data, dis, step = perception
+        # //TODO: If not possible to clean dirty 
+        # carrying a baby, check also the rollers
+        if not data[Tags.DIRTY]:
+            return False
+
+        # Body
+        data = data[Tags.DIRTY]
+        dirty = self.find(data, dis)
+    
+        self.save_plan(dirty.position, step)
+        return True
+
+    def save_plan(self, pos, step):
+        self.plan = []
+        x, y = self.position
+        for xdir, ydir in get_path(pos, step): 
+            x, y = x + xdir, y + ydir
+            self.plan.append(((x, y), self.env.get(x, y).tag()))
