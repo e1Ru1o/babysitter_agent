4f15fbc4088a0b295518b65c7ec1d3f62776c046
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-18 19:16:35 -0500

    [main][agents] - Adding a parameter for select the robot Agent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import House, Logger
+from src import House, Logger, get_robot
 from collections import defaultdict
 
 def get_level(level):
@@ -20,7 +20,8 @@ def main(args):
     # Running the env
     data = defaultdict(lambda: 0)
     for _ in range(args.repetitions):
-        env = House(args.rows, args.columns, args.time, args.babies, args.toys, args.dirty, args.cicles)
+        env = House(args.rows, args.columns, args.time, args.babies, \
+            args.toys, args.dirty, args.cicles, get_robot(args.robot))
         status = env.run()
         data['DIRTY-MEAN'] += env.dirty
         data[status.name]  += 1
@@ -34,16 +35,18 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser('Enviroment simulator')
-    parser.add_argument('-n',   '--rows',        type=int,   required=True,    help='Number of row of the enviroment')
-    parser.add_argument('-m',   '--columns',     type=int,   required=True,    help='Number of columns of the enviroment')
-    parser.add_argument('-b',   '--babies',      type=int,   required=True,    help='Number of babies in the enviroment')
-    parser.add_argument('-o',   '--toys',        type=float, required=True,    help='Percent of obstacles(toys) in the enviroment')
-    parser.add_argument('-d',   '--dirty',       type=float, required=True,    help='Percent of dirty cells in the enviroment')
-    parser.add_argument('-t',   '--time',        type=int,   required=True,    help='Enviroment life cicle duration')
-    parser.add_argument('-c',   '--cicles',      type=int,   default=100,      help='Number of cicles to run')
-    parser.add_argument('-rep', '--repetitions', type=int,   default=30,       help='Number of times to run the env')
-    parser.add_argument('-lvl', '--level',       type=str,   default='notset', help='Number of cicles to run')
-    parser.add_argument('-f',   '--log-file',    type=str,   default='',       help='File to write the logs')
+    # cmd = parser.add_subparsers('cmd', 'Command line Arguments')
+    parser.add_argument('-n',   '--rows',        type=int,   required=True,      help='Number of row of the enviroment')
+    parser.add_argument('-m',   '--columns',     type=int,   required=True,      help='Number of columns of the enviroment')
+    parser.add_argument('-b',   '--babies',      type=int,   required=True,      help='Number of babies in the enviroment')
+    parser.add_argument('-o',   '--toys',        type=float, required=True,      help='Percent of obstacles(toys) in the enviroment')
+    parser.add_argument('-d',   '--dirty',       type=float, required=True,      help='Percent of dirty cells in the enviroment')
+    parser.add_argument('-t',   '--time',        type=int,   required=True,      help='Enviroment life cicle duration')
+    parser.add_argument('-c',   '--cicles',      type=int,   default=100,        help='Number of cicles to run')
+    parser.add_argument('-rep', '--repetitions', type=int,   default=30,         help='Number of times to run the env')
+    parser.add_argument('-lvl', '--level',       type=str,   default='notset',   help='Number of cicles to run')
+    parser.add_argument('-f',   '--log-file',    type=str,   default='',         help='File to write the logs')
+    parser.add_argument('-bot', '--robot',       type=str,   default='reactive', help='File to write the logs')
 
     args = parser.parse_args()
     main(args)
--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -5,5 +5,17 @@ from .robot import Robot
 from .roller import Roller
 from .reactive import Reactive
 from .error import AgentError
+from .proactive import Proactive
 
 from .utils import DIR, ALL_DIR
+
+robots = [
+    Reactive,
+    Proactive,
+]
+
+def get_robot(name):
+    for robot in robots:
+        if robot.__name__.lower() == name.lower():
+            return robot
+    raise AgentError(f'Robot `{name}` implementation not found')
